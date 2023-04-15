cc133898e045bd3148c3b9f823bd9437316c7835
evgeniy-shpyra <foksmix256@gmail.com>
2023-04-15 18:52:22 +0300

    add selectors

--- src/components/selectors/Selecters.tsx
+++ src/components/selectors/Selecters.tsx
@@ -5,6 +5,7 @@ import RowContainer from "../rowContainer/RowContainer"
 import DeleteIcon from "../icons/deleteIcon/DeleteIcon"
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks"
 import { cancelSelection } from "../../redux/features/itemsSlice"
+import Selector from "./selector/Selector"
 
 const Selectors: React.FC = () => {
     const dispatch = useAppDispatch()
@@ -20,24 +21,25 @@ const Selectors: React.FC = () => {
     return (
         <div className={styles.rowContainer}>
             <RowContainer customStyles={styles.row}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <Selector />
+                <Selector />
+                <Selector />
+                <Selector />
             </RowContainer>
-
-            {selectedItemsId.length > 0 && (
-                <button
-                    onClick={onCancelSelectionHandler}
-                    className={styles.cancelSelectionButton}
-                >
-                    <DeleteIcon
-                        size='24px'
-                        color='rgba(0, 0, 0, 0.65)'
-                        hoverColor='rgba(0, 0, 0, 0.8)'
-                    />
-                </button>
-            )}
+            <div className={styles.buttonContainer}>
+                {selectedItemsId.length > 0 && (
+                    <button
+                        onClick={onCancelSelectionHandler}
+                        className={styles.cancelSelectionButton}
+                    >
+                        <DeleteIcon
+                            size='24px'
+                            color='rgba(0, 0, 0, 0.65)'
+                            hoverColor='rgba(0, 0, 0, 0.8)'
+                        />
+                    </button>
+                )}
+            </div>
         </div>
     )
 }
--- src/components/selectors/selector/Selector.tsx
+++ src/components/selectors/selector/Selector.tsx
@@ -1,8 +1,32 @@
 import React from "react"
 import styles from "./Selector.module.scss"
+import classNames from "classnames"
 
 const Selector: React.FC = () => {
-    return <div className={styles.container}>{/* <input type /> */}</div>
+    const [isActive, setIsActive] = React.useState(false)
+
+    const containerStyles = classNames(styles.container, {
+        [styles.active]: isActive,
+    })
+
+    const onFocusHandler = () => {
+        setIsActive(true)
+    }
+
+    const onBlurHandler = () => {
+        setIsActive(false)
+    }
+
+    return (
+        <div className={containerStyles}>
+            <input
+                type='text'
+                onFocus={onFocusHandler}
+                onBlur={onBlurHandler}
+                className={styles.input}
+            />
+        </div>
+    )
 }
 
 export default Selector
