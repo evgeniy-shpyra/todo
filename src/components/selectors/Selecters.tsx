561104e9ba0ffdd9a6b32264db4c6cb159e071ed
evgeniy-shpyra <foksmix256@gmail.com>
2023-04-15 19:01:35 +0300

    fixed clear button

--- src/components/selectors/Selecters.tsx
+++ src/components/selectors/Selecters.tsx
@@ -6,6 +6,7 @@ import DeleteIcon from "../icons/deleteIcon/DeleteIcon"
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks"
 import { cancelSelection } from "../../redux/features/itemsSlice"
 import Selector from "./selector/Selector"
+import classNames from "classnames"
 
 const Selectors: React.FC = () => {
     const dispatch = useAppDispatch()
@@ -18,6 +19,10 @@ const Selectors: React.FC = () => {
         dispatch(cancelSelection())
     }
 
+    const buttonContainerStyles = classNames(styles.buttonContainer, {
+        [styles.hiddenClearButton]: !(selectedItemsId.length > 0),
+    })
+
     return (
         <div className={styles.rowContainer}>
             <RowContainer customStyles={styles.row}>
@@ -26,19 +31,17 @@ const Selectors: React.FC = () => {
                 <Selector />
                 <Selector />
             </RowContainer>
-            <div className={styles.buttonContainer}>
-                {selectedItemsId.length > 0 && (
-                    <button
-                        onClick={onCancelSelectionHandler}
-                        className={styles.cancelSelectionButton}
-                    >
-                        <DeleteIcon
-                            size='24px'
-                            color='rgba(0, 0, 0, 0.65)'
-                            hoverColor='rgba(0, 0, 0, 0.8)'
-                        />
-                    </button>
-                )}
+            <div className={buttonContainerStyles}>
+                <button
+                    onClick={onCancelSelectionHandler}
+                    className={styles.cancelSelectionButton}
+                >
+                    <DeleteIcon
+                        size='24px'
+                        color='rgba(0, 0, 0, 0.65)'
+                        hoverColor='rgba(0, 0, 0, 0.8)'
+                    />
+                </button>
             </div>
         </div>
     )
